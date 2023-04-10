eb4e5c02617164e872494cd57ad5aa92a305ad07
omiras <oscar.miras@gmail.com>
2023-04-10 09:36:09 +0000

    numberstable

--- src/js/component/NumbersTable.jsx
+++ src/js/component/NumbersTable.jsx
@@ -1,25 +1,14 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
-
-export default function NumbersTable({ limit, color = 'red' }) {
+export default function NumbersTable({ limit }) {
  
-  if (limit <= 0) {
-    throw new RangeError('The limit number must be greather than 0.');
-  }
-
   const numbers = new Array(limit).fill(0);
   
   return (
     <div className="row">
       {numbers.map((n, index) => (
-        <div key={index} style={{backgroundColor: color}} className={`col-3 border text-center`}>{index + 1}</div>
+        <div className={`col-3 border text-center ${(index)%2!==0 && 'bg-danger'}`}>{index + 1}</div>
       ))}
     </div>
   );
 }
-
-NumbersTable.propTypes = {
-  limit: PropTypes.number.isRequired,
-  color: PropTypes.string
-};
